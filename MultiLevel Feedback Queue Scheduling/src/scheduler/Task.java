892b1288913882d8dbbaa64faff739ba73546790
Somshubra Majumdar <titu1994@gmail.com>
2014-10-04 10:53:42 +0530

    Implemented Task

--- MultiLevel Feedback Queue Scheduling/src/scheduler/Task.java
+++ MultiLevel Feedback Queue Scheduling/src/scheduler/Task.java
@@ -1,5 +1,30 @@
 package scheduler;
 
-public class Task {
+public class Task implements Comparable<Task> {
+	
+	public int burstTime;
+	public int arrivalTime;
+	public int deadlineTime;
+	
+	private int completedTime;
+	
+	public Task(int burstTime, int arrivalTime, int deadlineTime) {
+		this.burstTime = burstTime;
+		this.arrivalTime = arrivalTime;
+		this.deadlineTime = deadlineTime;
+	}
+
+	@Override
+	public int compareTo(Task o) {
+		return burstTime - o.burstTime;
+	}
+	
+	public int getCompletedTime() {
+		return completedTime;
+	}
+
+	public void setCompletedTime(int completedTime) {
+		this.completedTime = completedTime;
+	}
 
 }
